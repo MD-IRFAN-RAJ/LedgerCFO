30b240b7bed35239f9fb086445051f4a0f7e5300
MD IRFAN RAJ <mdirfanraj88.omega@gmail.com>
2026-03-22 23:59:58 +0530

    feat: implement express server with database integration

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import clientRoutes from "./routes/clientRoutes.js";
+import taskRoutes from "./routes/taskRoutes.js";
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(cors({ origin: process.env.FRONTEND_URL || "http://localhost:5173" }));
+app.use(express.json());
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+
+app.use("/api/clients", clientRoutes);
+app.use("/api/tasks", taskRoutes);
+
+// Connect DB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
